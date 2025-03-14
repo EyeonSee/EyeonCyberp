6b9d14459334325387b5ab53ced62ecb59b911cb
Ian Seetaram <ian.seetaram@gmail.com>
2025-03-14 03:37:13 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Resume from "./components/Resume";
 import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Certifications from "./components/Certifications";
+import Testimonials from "./components/Testimonials";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
@@ -11,7 +14,10 @@ const App = () => {
       <Navbar />
       <Hero />
       <Resume />
+      <Skills />
+      <Certifications />
       <Projects />
+      <Testimonials />
       <Contact />
       <Footer />
     </>
