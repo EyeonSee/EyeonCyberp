27d9aeb89362834c38ff11f7c08cd05a59e608cf
Ian Seetaram <ian.seetaram@gmail.com>
2025-03-15 03:36:23 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Resume from "./components/Resume";
-import Skills from "./components/Skills";
-import Projects from "./components/Projects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+// App.jsx
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Resume from './components/Resume';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="bg-[#0D1117] text-white">
-      <Navbar />
+      <Navbar /> {/* Render Navbar only once */}
       <Hero />
       <Resume />
       <Skills />
