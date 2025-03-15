8a8f85bfcd26649ad1baf92020519249465dad4d
Ian Seetaram <ian.seetaram@gmail.com>
2025-03-15 02:31:41 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 const Footer = () => {
   return (
-    <footer className="bg-[#0D1117] text-white text-center py-6 mt-10">
+    <footer className="bg-black/30 backdrop-blur-lg text-white text-center p-6 mt-10">
       <p className="text-sm">&copy; {new Date().getFullYear()} Ian Seetaram. All Rights Reserved.</p>
       <div className="mt-3 flex justify-center space-x-6 footer-links">
         <a href="https://linkedin.com/in/ian-seetaram" target="_blank" rel="noopener noreferrer" className="hover:text-blue-400 transition">
           LinkedIn
         </a>
-        <a href="https://github.com/yourgithub" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400 transition">
+        <a href="https://github.com/EyeonSee" target="_blank" rel="noopener noreferrer" className="hover:text-gray-400 transition">
           GitHub
         </a>
         <a href="mailto:ian.seetaram@pm.me" className="hover:text-red-400 transition">
